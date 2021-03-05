36319346a364c7c02fcb46c1b2fab494551fada8
Ivan Nikiforov <nikiforr@gmail.com>
2021-03-05 17:14:24 +0300

    Use map for O(1) access

--- models.wallets.go
+++ models.wallets.go
@@ -6,17 +6,15 @@ import (
 )
 
 // this is for demo only until we have not implemented DB layer
-var walletList = []wallet{
-	{1, decimal.NewFromFloat(12.34)},
-	{2, decimal.NewFromFloat(56.78)},
+var wallets = map[int]wallet{
+	1: {1, decimal.NewFromFloat(12.34)},
+	2: {2, decimal.NewFromFloat(56.78)},
 }
 
 // return a wallet with a specified ID
 func getWalletByID(id int) (*wallet, error) {
-	for _, w := range walletList {
-		if w.ID == id {
-			return &w, nil
-		}
+	if w, ok := wallets[id]; ok {
+		return &w, nil
 	}
 	return nil, errors.New("wallet not found")
 }
