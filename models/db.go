fb8c9e55d20ebb09f152814a0b782e555dd8e69f
Ivan Nikiforov <nikiforr@gmail.com>
2021-03-08 12:51:07 +0300

    Handle DB connection retry and Debug mode

--- models/db.go
+++ models/db.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/niki4/challenge_quick_wallet_api/types"
+	"github.com/shopspring/decimal"
 	"log"
 	"os"
 	"time"
@@ -25,13 +26,14 @@ func InitStorage() error {
 	if dbHost == "" {
 		dbHost = "localhost"
 	}
+	dbDebug := os.Getenv("DEBUG")
 	var err error
 
 	// To handle time.Time correctly, we included parseTime as a parameter to DSN.
 	// To fully support UTF-8 encoding, we changed charset=utf8 to charset=utf8mb4 in DSN.
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbName)
 
-	for i := 0; i < maxConnectRetries; i++ {
+	for i := 1; i < maxConnectRetries+1; i++ {
 		db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 		if err != nil {
 			log.Println("Error connecting to database: ", err)
@@ -39,12 +41,14 @@ func InitStorage() error {
 			log.Println("DB connection established")
 			break
 		}
-		if i == maxConnectRetries-1 {
+		if i == maxConnectRetries {
 			err = errors.New("couldn't connect to DB")
 			log.Println(err)
 			return err
 		}
-		time.Sleep(1 * time.Second)
+		waitSec := 3 * i
+		log.Println("Wait before retry DB connect:", waitSec, "seconds")
+		time.Sleep(time.Duration(waitSec) * time.Second)
 	}
 
 	// Migrate the schema (automatically create tables, missing columns and missing indexes, if needed)
@@ -52,6 +56,19 @@ func InitStorage() error {
 		err = errors.New("failed DB schema migration for Wallet")
 		log.Println(err)
 		return err
+	}
+
+	// Create some wallets for testing in DB if debug mode is ON
+	if dbDebug != "" {
+		walletsData := []types.Wallet{
+			{Balance: decimal.RequireFromString("100.00")},
+			{Balance: decimal.RequireFromString("50.00")},
+			{Balance: decimal.RequireFromString("500.00")},
+			{Balance: decimal.RequireFromString("1.00")},
+			{Balance: decimal.RequireFromString("0.05")},
+		}
+		db.CreateInBatches(walletsData, 5)
+		log.Printf("Test wallets has been created: \n%#v\n", walletsData)
 	}
 
 	return nil
