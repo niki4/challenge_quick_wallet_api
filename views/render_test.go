3fa0f7c37a7368ebc3693bc541f358473b248ab2
Ivan Nikiforov <nikiforr@gmail.com>
2021-03-09 15:03:12 +0300

    Assert for Render response

--- views/render_test.go
+++ views/render_test.go
@@ -1,8 +1,10 @@
 package views
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/niki4/challenge_quick_wallet_api/types"
 	"github.com/shopspring/decimal"
 	"github.com/stretchr/testify/assert"
 
@@ -12,9 +14,9 @@ import (
 )
 
 var testPayload = map[string]interface{}{
-	"payload": map[string]interface{}{
-		"id":      123,
-		"balance": decimal.RequireFromString("7.89"),
+	"payload": types.Wallet{
+		ID:      123,
+		Balance: decimal.RequireFromString("7.89"),
 	},
 }
 
@@ -55,6 +57,21 @@ func TestRenderJSON(t *testing.T) {
 		t.Run(fmt.Sprintf("%s: %s", tc.method, tc.name), func(t *testing.T) {
 			w := performRequest(router, tc.method, "/", tc.headers)
 			assert.Equal(t, tc.expStatus, w.Code)
+			if tc.method == http.MethodGet || tc.method == http.MethodPost {
+				if tc.expStatus == http.StatusOK {
+					var resp types.Wallet
+					if err := json.NewDecoder(w.Body).Decode(&resp); err != nil {
+						t.Error(err)
+					}
+					assert.Equal(t, testPayload["payload"], resp)
+				} else if tc.expStatus == http.StatusBadRequest {
+					var resp types.ApiError
+					if err := json.NewDecoder(w.Body).Decode(&resp); err != nil {
+						t.Error(err)
+					}
+					assert.Equal(t, types.ApiError{Error: "incorrect request"}, resp)
+				}
+			}
 		})
 	}
 }
